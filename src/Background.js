cad5ffbb059280fae19263cd90509fab0abc6a8e
pearlSlugs <61484145+pearlslugs@users.noreply.github.com>
2020-06-29 13:11:13 -0600

    contact

--- src/Background.js
+++ src/Background.js
@@ -6,6 +6,16 @@ function Background() {
     <div className="Background">
 	<h1 className="text-front-page">Garrett McClung</h1>
 	<h2 id="under-text-front-page">Web Dev</h2>
+	<h3>307-286-8545</h3>
+	<h3>garrettmcclung1@gmail.com</h3>
+	<div className="social">
+	<a href="https://www.facebook.com/gary.mcclung.12" target="_blank" rel="noopener noreferrer">
+		<div className="twitter"></div>
+	</a>
+		<a href="https://twitter.com/pearlslugs" target="_blank" rel="noopener noreferrer">
+			<div className="facebook"></div>
+		</a>
+	</div>
     </div>
   );
 }
